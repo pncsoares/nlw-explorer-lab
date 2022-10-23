9ddbcd8de79e2ebfa872fd6abce7515a18c872cc
Pedro Soares <pncsoares@gmail.com>
2022-10-23 16:56:42 +0100

    Add security code and card number validations

--- src/main.js
+++ src/main.js
@@ -1,30 +1,126 @@
+import IMask from 'imask';
+
 import './css/index.css';
 
-/**
- * 1. Select class with name cc-bg
- * 2. Select svg
- * 3. Select first level g element
- * 4. Select first child g element
- * 5. Select its path
- */
-const cardBackgroundColor01 = document.querySelector(
-    '.cc-bg svg > g g:nth-child(1) path'
-);
+function handleCardType() {
+    const colors = {
+        visa: ['#436D99', '#2D57F2'],
+        mastercard: ['#DF6F29', '#C69347'],
+        default: ['black', 'gray'],
+    };
 
-const cardBackgroundColor02 = document.querySelector(
-    '.cc-bg svg > g g:nth-child(2) path'
-);
+    /**
+     * 1. Select class with name cc-bg
+     * 2. Select svg
+     * 3. Select first level g element
+     * 4. Select first child g element
+     * 5. Select its path
+     */
+    const cardBackgroundColor01 = document.querySelector(
+        '.cc-bg svg > g g:nth-child(1) path'
+    );
+    const cardBackgroundColor02 = document.querySelector(
+        '.cc-bg svg > g g:nth-child(2) path'
+    );
+    const cardLogo = document.querySelector('.cc-logo span:nth-child(2) img');
 
-const cardLogo = document.querySelector('.cc-logo span:nth-child(2) img');
-
-const colors = {
-    visa: ['#436D99', '#2D57F2'],
-    mastercard: ['#DF6F29', '#C69347'],
-    default: ['black', 'gray'],
-};
-
-function setCardType(type) {
-    cardBackgroundColor01.setAttribute('fill', colors[type][0]);
-    cardBackgroundColor02.setAttribute('fill', colors[type][1]);
-    cardLogo.setAttribute('src', `cc-${type}.svg`);
+    function setCardType(type) {
+        cardBackgroundColor01.setAttribute('fill', colors[type][0]);
+        cardBackgroundColor02.setAttribute('fill', colors[type][1]);
+        cardLogo.setAttribute('src', `cc-${type}.svg`);
+    }
 }
+
+function handleCardNumber() {
+    const cardNumber = document.querySelector('#card-number');
+
+    /**
+     * VISA
+     * starts with 4 and have a total of 15 numbers
+     */
+    const visaRegex = /^4\d{0,15}/;
+
+    /**
+     * MASTERCARD
+     *  starts with 5 followed by a number between 1 and 5 followed by two more numbers followed by 12 more numbers
+     *   OR
+     *  starts with 22 followed by a number between 2 and 9 followed by one more number followed by 12 more numbers
+     *   OR
+     *  starts with 2 followed by a number between 3 and 7 followed by two more numbers followed by 12 more numbers
+     */
+    const mastercardRegex =
+        /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/;
+
+    const cardNumberPattern = {
+        mask: [
+            {
+                mask: '0000 0000 0000 0000',
+                regex: visaRegex,
+                cardType: 'visa',
+            },
+            {
+                mask: '0000 0000 0000 0000',
+                regex: mastercardRegex,
+                cardType: 'mastercard',
+            },
+            {
+                mask: '0000 0000 0000 0000',
+                cardType: 'default',
+            },
+        ],
+        dispatch: (appended, dynamicMasked) => {
+            const number = (dynamicMasked.value + appended).replace(/\D/g, '');
+
+            return dynamicMasked.compiledMasks.find(({ regex }) => {
+                return number.match(regex);
+            });
+        },
+    };
+
+    IMask(cardNumber, cardNumberPattern);
+}
+
+function handleCardholderName() {}
+
+function handleExpiry() {
+    const expirationDate = document.querySelector('#expiration-date');
+    const currentYear = new Date().getFullYear();
+
+    const expirationDatePattern = {
+        mask: 'MM{/}YYYY',
+        blocks: {
+            MM: {
+                mask: IMask.MaskedRange,
+                from: 1,
+                to: 12,
+            },
+            YYYY: {
+                mask: IMask.MaskedRange,
+                from: currentYear,
+                to: currentYear + 10,
+            },
+        },
+    };
+
+    IMask(expirationDate, expirationDatePattern);
+}
+
+function handleCardSecurityCode() {
+    const securityCode = document.querySelector('#security-code');
+
+    const securityCodePattern = {
+        mask: '000',
+    };
+
+    IMask(securityCode, securityCodePattern);
+}
+
+function init() {
+    handleCardType();
+    handleCardNumber();
+    handleCardholderName();
+    handleExpiry();
+    handleCardSecurityCode();
+}
+
+init();
